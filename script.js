f85cf0091dd0ee14830bb52a0045d6b4778e3f46
AEOQ <200552650+AEOQ@users.noreply.github.com>
2025-09-16 12:19:28 +0800

    scroll

--- script.js
+++ script.js
@@ -45,11 +45,8 @@ class PointerInteraction { // #private  $data  _user
         this.#events.pointerup = this.#events.pointercancel = () => this.#lift();
     }
     #drag (ev) {
-        if (this._scroll) 
-            return ev.pointerType == 'mouse' && this.drag.to.scroll(this._scroll === true ? undefined : this._scroll);
-        if (this.target.Q('.PI-target')) 
-            return this.#reset();
-        ev.preventDefault();
+        this._scroll && (ev.pointerType != 'mouse') ? null : ev.preventDefault();
+        if (this.target.Q('.PI-target')) return this.#reset();
         
         this.$drag = {
             ...this.$drag ?? {tx: 0, ty: 0},
@@ -59,6 +56,7 @@ class PointerInteraction { // #private  $data  _user
         this.target.classList.add('PI-dragged');
         
         this.#hold.timer?.forEach(clearTimeout);
+        this._scroll && ev.pointerType == 'mouse' && this.drag.to.scroll(this._scroll === true ? undefined : this._scroll);
         if (this._drop) {
             this.drag.to.translate({x: this._drag?.x, y: this._drag?.y});
             this.drag.to.scrollPage();
